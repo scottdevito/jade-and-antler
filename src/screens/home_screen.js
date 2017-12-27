cbefd341948fa7a13e678b9259b5810498b8150b
Scott DeVito <scottadevito@gmail.com>
2017-12-26 20:19:09 -0500

    Add initial HomeScreen

---
+++ src/screens/home_screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <h1 className="App-title">JADE & ANTLER</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default HomeScreen;
