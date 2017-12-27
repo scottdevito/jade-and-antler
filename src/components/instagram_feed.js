7a394b0b3cc084fc989ad53ecbbe7543e455d4dc
Scott DeVito <scottadevito@gmail.com>
2017-12-27 18:33:48 -0500

    Finish InstagramFeed and add mock data

--- src/components/instagram_feed.js
+++ src/components/instagram_feed.js
@@ -1,10 +1,44 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import { GridList, GridListTile } from 'material-ui/GridList';
+import { tileData } from '../images/mockIgPics/tileData';
 
-class InstagramFeed extends Component {
-  render() {
-    return <div>Instagram Feed</div>;
-  }
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    background: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '70vw',
+    // height: 450,
+  },
+  subheader: {
+    width: '100%',
+  },
+});
+
+function InstagramFeed(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={160} className={classes.gridList} cols={3}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img} cols={tile.cols || 1}>
+            <img src={tile.img} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
 
-export default InstagramFeed;
+InstagramFeed.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(InstagramFeed);
