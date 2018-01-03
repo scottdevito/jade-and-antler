83c95dc126ef58eaed690e71e46db5ce85e61225
Scott DeVito <scottadevito@gmail.com>
2018-01-02 22:29:18 -0500

    Begin adding dynamic month headers to WorkshopsScreen

--- src/screens/workshops_screen.js
+++ src/screens/workshops_screen.js
@@ -4,17 +4,33 @@ import styled from 'styled-components';
 import WorkshopCard from '../components/workshops/workshop_card';
 import Footer from '../components/footer';
 
+import JanImg from '../images/workshopHeaders/jan.jpg';
+import FebImg from '../images/workshopHeaders/feb.jpg';
+import MarImg from '../images/workshopHeaders/mar.jpg';
+import AprImg from '../images/workshopHeaders/apr.jpg';
+import MayImg from '../images/workshopHeaders/may.jpg';
+
 class WorkshopsScreen extends Component {
   render() {
     return (
       <div>
         <StyledScreenWrapper>
-          <StyledMonthHeader>January 2018</StyledMonthHeader>
+          <StyledJanHeader>January 2018</StyledJanHeader>
+          <WorkshopCard />
+          <WorkshopCard />
+          <StyledFebHeader>Febrary 2018</StyledFebHeader>
           <WorkshopCard />
           <WorkshopCard />
           <WorkshopCard />
+          <StyledMarHeader>March 2018</StyledMarHeader>
           <WorkshopCard />
-          <StyledMonthHeader>Febrary 2018</StyledMonthHeader>
+          <WorkshopCard />
+          <StyledAprHeader>April 2018</StyledAprHeader>
+          <WorkshopCard />
+          <WorkshopCard />
+          <WorkshopCard />
+          <StyledMayHeader>May 2018</StyledMayHeader>
+          <WorkshopCard />
           <WorkshopCard />
           <WorkshopCard />
         </StyledScreenWrapper>
@@ -45,15 +61,38 @@ const StyledMonthHeader = styled.div`
   align-items: center;
   color: #ffffff;
   background-color: #b29e93;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
   font-weight: bold;
   font-size: 1.75em;
   height: 4em;
   width: 100%;
-  margin-top: 1em;
-  margin-bottom: 1em;
 
   @media (max-width: 767px) {
     font-size: 1.5em;
     height: 5em;
   }
+`;
+
+const StyledJanHeader = StyledMonthHeader.extend`
+  background-image: url('${JanImg}');
+`;
+
+const StyledFebHeader = StyledMonthHeader.extend`
+  background-image: url('${FebImg}');
+`;
+
+const StyledMarHeader = StyledMonthHeader.extend`
+  background-image: url('${MarImg}');
+`;
+
+const StyledAprHeader = StyledMonthHeader.extend`
+  background-image: url('${AprImg}');
+`;
+
+const StyledMayHeader = StyledMonthHeader.extend`
+  background-image: url('${MayImg}');
 `;
