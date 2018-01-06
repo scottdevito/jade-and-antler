c44a30dca438fd140aaa3979d029f8da960143c4
Scott DeVito <scottadevito@gmail.com>
2018-01-06 13:14:36 -0500

    Extract Workshops mock data to seperate file

---
+++ src/components/workshops/workshops_mock_data.js
@@ -0,0 +1,29 @@
+export const Workshops = [
+  {
+    eventId: '0001',
+    name: 'Yoga and Meditation Workshop',
+    month: 'January',
+    day: 8,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0002',
+    name: 'Yoga and Meditation Workshop',
+    month: 'January',
+    day: 10,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0003',
+    name: 'Yoga and Meditation Workshop',
+    month: 'February',
+    day: 8,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+];
