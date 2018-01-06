4498d9544c4afa10c4ad129c65ff9702249a7983
Scott DeVito <scottadevito@gmail.com>
2018-01-06 13:25:52 -0500

    Add more mock workshops

--- src/components/workshops/workshops_mock_data.js
+++ src/components/workshops/workshops_mock_data.js
@@ -26,4 +26,94 @@ export const Workshops = [
     description:
       'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
   },
+  {
+    eventId: '0004',
+    name: 'Yoga and Meditation Workshop',
+    month: 'February',
+    day: 17,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0005',
+    name: 'Yoga and Meditation Workshop',
+    month: 'March',
+    day: 2,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0006',
+    name: 'Yoga and Meditation Workshop',
+    month: 'March',
+    day: 8,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0007',
+    name: 'Yoga and Meditation Workshop',
+    month: 'March',
+    day: 25,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0008',
+    name: 'Yoga and Meditation Workshop',
+    month: 'April',
+    day: 1,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0009',
+    name: 'Yoga and Meditation Workshop',
+    month: 'April',
+    day: 27,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0010',
+    name: 'Yoga and Meditation Workshop',
+    month: 'April',
+    day: 28,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0011',
+    name: 'Yoga and Meditation Workshop',
+    month: 'May',
+    day: 3,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0012',
+    name: 'Yoga and Meditation Workshop',
+    month: 'May',
+    day: 13,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
+  {
+    eventId: '0013',
+    name: 'Yoga and Meditation Workshop',
+    month: 'May',
+    day: 22,
+    year: 2018,
+    description:
+      'Join us for a 2 hour session of Vinyassa yoga and mediation on a Sunday afternoon. This is a long card for length.',
+  },
 ];
