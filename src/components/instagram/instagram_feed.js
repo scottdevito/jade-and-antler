7907e265075b4e3cafa86bf44b0eebc40659d3f2
Scott DeVito <scottadevito@gmail.com>
2018-01-17 18:56:02 -0500

    Strip away material-ui GridList and use flexbox

--- src/components/instagram/instagram_feed.js
+++ src/components/instagram/instagram_feed.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GridList, GridListTile } from 'material-ui/GridList';
 import { tileData } from '../../images/mockIgPics/tileData';
 
 // import InstagramFeedHeader from './instagram_feed_header';
@@ -8,13 +7,10 @@ import { tileData } from '../../images/mockIgPics/tileData';
 function InstagramFeed(props) {
   return (
     <StyledGridListContainer>
-      <StyledGridList cols={3}>
-        {tileData.map(tile => (
-          <StyledTile key={tile.img} cols={tile.cols || 1}>
-            <img src={tile.img} alt={tile.title} />
-          </StyledTile>
-        ))}
-      </StyledGridList>
+      {tileData.map(tile => {
+        console.log(tile.img);
+        <StyledTile key={tile.img} imgSrc={tile.img} />;
+      })}
     </StyledGridListContainer>
   );
 }
@@ -23,13 +19,15 @@ export default InstagramFeed;
 
 const StyledGridListContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   width: 65vw;
   /* height: 250 */
   margin-top: 10em;
   margin-bottom: 16em;
+  flex-wrap: wrap;
+  overflow: hidden;
 
   @media (max-width: 767px) {
     margin-top: 6em;
@@ -37,15 +35,13 @@ const StyledGridListContainer = styled.div`
   }
 `;
 
-const StyledGridList = styled(GridList)`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  overflow: hidden;
-`;
-
-const StyledTile = styled(GridListTile)`
-  height: 220px;
+const StyledTile = styled.div`
+  height: 100px;
+  background-image: url('${props => props.imgSrc}');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
 
   @media (max-width > 425px && max-width < 769px) {
     height: 100px;
