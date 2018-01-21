ee58d19920fc8465cc60332e5cf2596c79998b5e
Scott DeVito <scottadevito@gmail.com>
2018-01-21 14:01:46 -0500

    Persist emailSubmitted state to localStorage
    https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage

--- src/index.js
+++ src/index.js
@@ -8,14 +8,25 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import thunk from 'redux-thunk';
+import { loadState, saveState } from './localStorage';
 
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Persist Redux state in localStorage
+const persistedState = loadState();
+
 const store = createStore(
   reducers,
+  persistedState,
   composeEnhancers(applyMiddleware(thunk, promiseMiddleware))
 );
+
+// Save Redux state in localStorage on state change
+store.subscribe(() => {
+  saveState({ emailSubmitted: store.getState().emailSubmitted });
+});
 
 ReactDOM.render(
   <Provider store={store}>
